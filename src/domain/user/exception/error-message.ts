52cd8de5b9f0e6f5c9efee8a979d61251ce09681
정승민 <sm.jeong@chabot.co.kr>
2022-07-03 02:35:44 +0900

    Feature: 유저 도메인 작성 #1
    - 유저 엔티티 작성
    - 유저 서비스 작성

---
+++ src/domain/user/exception/error-message.ts
@@ -0,0 +1,3 @@
+export class EntityNotFoundExceptionMessage {
+  static readonly UserNotFoundMessage = '유저를 찾을 수 없어요';
+}
---
+++ src/domain/user/service/user.service.spec.ts
@@ -0,0 +1,101 @@
+import { Provider } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserDITokens } from '../di/user-di-tokens';
+import { UserReader } from '../repository/user.reader';
+import { UserStore } from '../repository/user.store';
+import { User } from '../user';
+import { UserServiceImpl } from './user.service';
+import { UserAlreadyExistsException } from '../../../common/exception/user-exception';
+
+class UserMemoryRepository implements UserStore, UserReader {
+  private users: User[] = [
+    new User({
+      name: '이름',
+      nickname: '닉네임',
+      email: '이메일@도메인.com',
+      phone: '010-1234-5678',
+      password: '비밀번호',
+    }),
+  ];
+
+  async store(user: User): Promise<User> {
+    if (
+      this.users.filter((u) => u.email === user.email || u.phone === user.phone)
+        .length > 0
+    )
+      throw new UserAlreadyExistsException();
+
+    this.users.push(user);
+    return user;
+  }
+
+  async findByEmailOrPhone(emailOrPhone: string): Promise<User> {
+    return this.users.filter(
+      (user) => user.email === emailOrPhone || user.phone === emailOrPhone,
+    )[0];
+  }
+
+  async findById(id: string): Promise<User> {
+    console.log(this.users);
+    return this.users.filter((user) => user.id === id)[0];
+  }
+}
+
+const persistenceProviders: Provider[] = [
+  {
+    provide: UserDITokens.UserStore,
+    useClass: UserMemoryRepository,
+  },
+  {
+    provide: UserDITokens.UserReader,
+    useClass: UserMemoryRepository,
+  },
+];
+
+describe('UserServiceImpl', () => {
+  let service: UserServiceImpl;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserServiceImpl, ...persistenceProviders],
+    }).compile();
+
+    service = module.get<UserServiceImpl>(UserServiceImpl);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('register user', async () => {
+    // given
+    const command = {
+      name: '이름',
+      nickname: '닉네임',
+      email: '이메일2@도메인.com',
+      phone: '010-1234-5679',
+      password: '비밀번호',
+    };
+    // when
+    const result = await service.registerUser(command);
+
+    // then
+    expect(result.id).toBeDefined();
+  });
+
+  it('register user with already exists email', async () => {
+    // given
+    const command = {
+      name: '이름',
+      nickname: '닉네임',
+      email: '이메일@도메인.com',
+      phone: '010-1234-5678',
+      password: '비밀번호',
+    };
+    // when
+    await service.registerUser(command).catch((error) => {
+      //then
+      expect(error).toBeInstanceOf(UserAlreadyExistsException);
+    });
+  });
+});
